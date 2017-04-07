c714281807ba5cf327c8ccb51ef3d05d85b9116d
Jayson Grace <jayson.e.grace@gmail.com>
2017-04-07 17:19:09 -0600

    Updated sqlmap and installDotFiles so that sqlmap results will be saved to /Users/l/.sqlmap as would be normally expected

--- installDotFiles.sh
+++ installDotFiles.sh
@@ -18,9 +18,16 @@ declare -a files=("bashutils" "docker" "osx" "aws" "python")
 
 # Used by the docker msfconsole and msfvenom containers
 installMetasploit(){
-    if [ ! -d "$HOME/metasploit-framework" ]; then
-        git clone git://github.com/rapid7/metasploit-framework.git $HOME/metasploit-framework
-    fi
+  if [ ! -d "$HOME/metasploit-framework" ]; then
+    git clone git://github.com/rapid7/metasploit-framework.git $HOME/metasploit-framework
+  fi
+}
+
+# Creates sqlmap folder if it doesn't already exist
+sqlmapFolder(){
+  if [ ! -d "$HOME/.sqlmap" ]; then
+    mkdir $HOME/.sqlmap
+  fi
 }
 
 # Backup old zshrc (if one exists)
@@ -57,3 +64,4 @@ done
 echo $installDir >> $dotdir/.dotinstalldir
 
 installMetasploit
+sqlmapFolder
